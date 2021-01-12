4d9d2233c986a07dd5d882208fa27d009677103f
Mark <mark@Marks-MacBook-Air.local>
2021-01-12 17:58:31 -0500

    made react app with scaffolding

---
+++ react-github-user-card/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import UserCard from "./Components/UserCard"
+import UserData from "./Components/UserData"
+
+class App extends React.Component {
+  
+
+  render() {
+    return (
+        <div>
+            <h1>Github Follower Profiles</h1>
+        </div>
+    )
+  }
+}
+
+export default App
