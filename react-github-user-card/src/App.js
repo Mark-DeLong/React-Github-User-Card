0d425afc2990aa8f4014162ff4eed9cf3ab7fd30
Mark <mark@Marks-MacBook-Air.local>
2021-01-12 23:54:13 -0500

    lots of stuff

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,17 +1,78 @@
 import React from 'react'
 import UserCard from "./Components/UserCard"
-import UserData from "./Components/UserData"
+import List from "./Components/List"
+import axios from "axios"
 
 class App extends React.Component {
   
+  constructor() {
+    super();
+    this.state = {
+      userData: {},
+      followers: [],
+      followerData: []
+    }
+  }
+
+  componentDidMount(){
+    
+    axios.get(`https://api.github.com/users/Mark-DeLong`)
+      .then(res => {
+        this.setState({
+          userData: res
+        })
+      })
+    }
+    
+  componentDidUpdate(prevProps, prevState){
+    
+    if (this.state.mainUserData !== prevState.mainUserData) {  
+      axios.get(`https://api.github.com/users/Mark-DeLong/followers`)
+        .then(res => {
+          console.log(res)
+        this.setState({
+          followers: res
+        })
+      })
+    }
+
+    if (this.state.followerData !== prevState.followerData) {
+      this.state.followerData.map(follower => {
+        return(
+        axios.get(`https://api.github.com/users/${followers}}`)
+          .then(res => {
+            console.log(res)
+          this.setState({
+            followerData: [...this.state.followerData, res]
+          })
+        })
+      )})
+    }
+  }
+    
 
   render() {
     return (
-        <div>
-            <h1>Github Follower Profiles</h1>
+      <div className="container">
+        <div 
+          className="title"
+        >
+          <h1>
+            Github Follower Profiles
+          </h1>
         </div>
+        <div 
+          className="followers"
+        >
+          <h2>
+            Your followers: 
+          </h2>
+          <List state={this.state} />
+        </div>
+      </div>
     )
   }
 }
+
 
 export default App
--- react-github-user-card/src/Components/UserCard.js
+++ react-github-user-card/src/Components/UserCard.js
@@ -1,14 +1,57 @@
-import React from "react"
+import React, { useState } from "react"
+import axios from "axios"
 
-const UserCard = props => {
+class UserCard extends React.Component {
+  render() {
+    
     return (
-      <div
-        // style={props.todo.completed ? { textDecoration: 'line-through' } : null}
-        // onClick={() => props.handleToggleComplete(props.todo.id)}
+      <div 
+        className="userCard"
       >
-
+        <img
+          href={this.props.user.avatar_url}
+        >
+        </img>
+        <div 
+          className="card-info"
+        >
+          <h3 
+            className="name"
+          >
+            {this.props.user.name}
+          </h3>
+          <p 
+            className="username"
+          >
+            {this.props.user.login}
+          </p>
+          <p 
+            className="location"
+          >
+            {this.props.user.location}
+          </p>
+          <p>
+            Profile: 
+            <a 
+              href={this.props.user.html_url}
+            >
+              {this.props.user.html_url}
+            </a>
+          </p>
+          <p>
+            Followers: {this.props.user.followers}
+          </p>
+          <p>
+            Following: {this.props.user.following}
+          </p>
+          <p>
+            Bio: {this.props.user.bio}
+          </p>
+        </div>
       </div>
     )
-  }
+   }
+}
+
 
 export default UserCard
